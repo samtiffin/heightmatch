ff7e5040f09bed5ce30af91f8311dc404d7d0e4c
samtiffin <sam@samtiffin.com>
2014-02-05 17:13:14 +0000

    Some source rejiggery and added support for onResize height matching

--- src/jquery.heightmatch.js
+++ src/jquery.heightmatch.js
@@ -1,13 +1,11 @@
 /**
 # TODO
-- optional bind to resize
 - height match groups
 - refresh on element add/remove
 - watch height of elements
 - trigger to refresh
-- destroy
 **/
-(function( $ ) {
+(function( window, $ ) {
     'use strict';
 
     function Heightmatch( elements, options ) {
@@ -18,26 +16,58 @@
         this.init();
     }
 
-    Heightmatch.defaults = {};
+    Heightmatch.defaults = {
+        bindResize: false
+    };
 
     var maxHeight = function( $elements ) {
-        return Math.max.apply(Math, $elements.map(function( i, element ) {  return $(element).outerHeight(true); }));
+        return Math.max.apply(Math, $elements.map(function( i, element ) {  return $(element).height('').outerHeight(true); }));
+    },
+
+    bindEvents = function() {
+        if (this.options.bindResize) {
+            $(window).on('resize.heightmatch', $.proxy(resizeHandler, this));
+        }
+    },
+
+    unbindEvents = function() {
+        if (this.options.bindResize) {
+            $(window).off('.heightmatch');
+        }
+    },
+
+    resizeHandler = function() {
+        this.matchHeights();
     };
 
     Heightmatch.prototype.init = function() {
+        bindEvents.call(this);
+
+        this.matchHeights();
+    };
+
+    Heightmatch.prototype.matchHeights = function() {
         this.$elements.height(maxHeight(this.$elements));
     };
 
-    Heightmatch.prototype.destroy = function() {};
+    Heightmatch.prototype.unmatchHeights = function() {
+        this.$elements.height('');
+    };
+
+    Heightmatch.prototype.destroy = function() {
+        unbindEvents.call(this);
+
+        this.unmatchHeights();
+    };
 
     $.fn.heightmatch = function( options ) {
         var name = 'heightmatch';
 
-        if (!$.data(this, name)) {
-            $.data(this, name, new Heightmatch(this, options));
+        if (!this.data(name)) {
+            this.data(name, new Heightmatch(this, options));
         }
 
         return this;
     };
 
-})( jQuery );
+})( window, jQuery );
