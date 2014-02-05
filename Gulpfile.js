267397f5f31c2b919a39db88dd382c67ef4daa48
samtiffin <sam@samtiffin.com>
2014-02-05 12:06:34 +0000

    Added comment banner to dist source

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,24 @@
+/* global require */
 var gulp   = require('gulp'),
+    header = require('gulp-header'),
     jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify')
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
+var pkg = require('./package.json');
+
+var banner = [
+    '/*!',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''
+].join('\n');
+
 var src     = 'jquery.heightmatch.js',
-    dist    = 'jquery.heightmatch.min.js'
+    dist    = 'jquery.heightmatch.min.js',
     srcDir  = './src/',
     distDir = './dist/';
 
@@ -17,11 +31,18 @@ gulp.task('lint', function() {
 gulp.task('uglify', function() {
     gulp.src(srcDir + src)
         .pipe(uglify({ outSourceMaps: true }))
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(rename(dist))
         .pipe(gulp.dest(distDir));
 });
 
-gulp.task('build', ['lint', 'uglify']);
+gulp.task('dist', function() {
+    gulp.src(srcDir + src)
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest(distDir));
+});
+
+gulp.task('build', ['lint', 'uglify', 'dist']);
 
 gulp.task('watch', function() {
    gulp.watch(srcDir + src, ['build']);
---
+++ dist/jquery.heightmatch.js
@@ -0,0 +1,49 @@
+/*!
+ * heightmatch - Super simple jQuery plugin that ensures elements have matching heights.
+ * @version v0.0.1
+ * @link https://github.com/samtiffin/heightmatch
+ * @license ISC
+ */
+/**
+# TODO
+- optional bind to resize
+- height match groups
+- refresh on element add/remove
+- watch height of elements
+- trigger to refresh
+- destroy
+**/
+(function( $ ) {
+    'use strict';
+
+    function Heightmatch( elements, options ) {
+        this.$elements = $(elements);
+
+        this.options = $.extend({}, Heightmatch.defaults, options);
+
+        this.init();
+    }
+
+    Heightmatch.defaults = {};
+
+    var maxHeight = function( $elements ) {
+        return Math.max.apply(Math, $elements.map(function( i, element ) {  return $(element).outerHeight(true); }));
+    };
+
+    Heightmatch.prototype.init = function() {
+        this.$elements.height(maxHeight(this.$elements));
+    };
+
+    Heightmatch.prototype.destroy = function() {};
+
+    $.fn.heightmatch = function( options ) {
+        var name = 'heightmatch';
+
+        if (!$.data(this, name)) {
+            $.data(this, name, new Heightmatch(this, options));
+        }
+
+        return this;
+    };
+
+})( jQuery );
